1a32253553f9865503a58481563c652f8e66c206
The Anomaly <favouradekogbe@gmail.com>
2021-01-02 13:56:35 +0100

    Writer Dashboard - changes to mobile nav

--- Writer Dashboard/js-file.js
+++ Writer Dashboard/js-file.js
@@ -1,9 +1,24 @@
+// function openMenu() {
+//   var x = document.getElementById("menu");
+//   // x.style.display = "none";
+//   console.log(x);
+//   console.log(x.style.display);
+//     if(x.style.display === "none") {
+//       return x.style.display = "block";
+//     } 
+//     else {
+//       return x.style.display = "none";
+//     }
+//   }
+
 function openMenu() {
-    var x = document.getElementById("menu");
-    if(x.style.display === "none") {
-      x.style.display = "block"
+  var x = document.getElementById("menu");
+  console.log(x);
+  console.log(x.classList);
+    if(x.classList.contains("hide") === true) {
+      x.classList.toggle("show");
     } 
     else {
-      x.style.display = "none";
+      return x.classList.toggle("hide");
     }
   }
